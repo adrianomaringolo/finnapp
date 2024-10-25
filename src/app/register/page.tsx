bb747961a6c8ee893924b17a2b705d49b4bb4b19
Adriano Gomes <agomes@fanatics.com>
2024-10-25 14:50:02 -0300

    feat: login and register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,129 +1,108 @@
-"use client";
+'use client'
 
-import { FormEvent, useState } from "react";
-import Link from "next/link";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { app } from "../../firebase";
-import { useRouter } from "next/navigation";
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
+import { useRouter } from 'next/navigation'
+import { FormEvent, useState } from 'react'
+import { app } from '../../firebase'
 
 export default function Register() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmation, setConfirmation] = useState("");
-  const [error, setError] = useState("");
-  const router = useRouter();
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [confirmation, setConfirmation] = useState('')
+	const [error, setError] = useState('')
+	const router = useRouter()
 
-  async function handleSubmit(event: FormEvent) {
-    event.preventDefault();
+	async function handleSubmit(event: FormEvent) {
+		event.preventDefault()
 
-    setError("");
+		setError('')
 
-    if (password !== confirmation) {
-      setError("Passwords don't match");
-      return;
-    }
+		if (password !== confirmation) {
+			setError("Passwords don't match")
+			return
+		}
 
-    try {
-      await createUserWithEmailAndPassword(getAuth(app), email, password);
-      router.push("/login");
-    } catch (e) {
-      setError((e as Error).message);
-    }
-  }
+		try {
+			await createUserWithEmailAndPassword(getAuth(app), email, password)
+			router.push('/login')
+		} catch (e) {
+			setError((e as Error).message)
+		}
+	}
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-8">
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-            Pray tell, who be this gallant soul seeking entry to mine humble
-            abode?
-          </h1>
-          <form
-            onSubmit={handleSubmit}
-            className="space-y-4 md:space-y-6"
-            action="#"
-          >
-            <div>
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Your email
-              </label>
-              <input
-                type="email"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="name@company.com"
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                id="password"
-                placeholder="••••••••"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="confirm-password"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Confirm password
-              </label>
-              <input
-                type="password"
-                name="confirm-password"
-                value={confirmation}
-                onChange={(e) => setConfirmation(e.target.value)}
-                id="confirm-password"
-                placeholder="••••••••"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required
-              />
-            </div>
-            {error && (
-              <div
-                className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-                role="alert"
-              >
-                <span className="block sm:inline">{error}</span>
-              </div>
-            )}
-            <button
-              type="submit"
-              className="w-full text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-primary-800"
-            >
-              Create an account
-            </button>
-            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-              Already have an account?{" "}
-              <Link
-                href="/login"
-                className="font-medium text-gray-600 hover:underline dark:text-gray-500"
-              >
-                Login here
-              </Link>
-            </p>
-          </form>
-        </div>
-      </div>
-    </main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center p-8">
+			<div className="max-w-4xl mx-auto p-6">
+				<div className="text-center mb-16">
+					<a href="javascript:void(0)">
+						<img
+							src="https://readymadeui.com/readymadeui.svg"
+							alt="logo"
+							className="w-52 inline-block"
+						/>
+					</a>
+					<h4 className="text-gray-800 text-base font-semibold mt-6">
+						Sign up into your account
+					</h4>
+				</div>
+
+				<form onSubmit={handleSubmit} className="md:w-full">
+					<div className="grid gap-8">
+						<div>
+							<label className="text-gray-800 text-sm mb-2 block">Email</label>
+							<input
+								name="email"
+								type="text"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+								className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3.5 rounded-md focus:bg-transparent outline-blue-500 transition-all"
+								placeholder="Enter email"
+							/>
+						</div>
+
+						<div>
+							<label className="text-gray-800 text-sm mb-2 block">Password</label>
+							<input
+								name="password"
+								type="password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3.5 rounded-md focus:bg-transparent outline-blue-500 transition-all"
+								placeholder="Enter password"
+							/>
+						</div>
+						<div>
+							<label className="text-gray-800 text-sm mb-2 block">Confirm Password</label>
+							<input
+								name="cpassword"
+								type="password"
+								value={confirmation}
+								onChange={(e) => setConfirmation(e.target.value)}
+								className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3.5 rounded-md focus:bg-transparent outline-blue-500 transition-all"
+								placeholder="Enter confirm password"
+							/>
+						</div>
+					</div>
+
+					{error && (
+						<div
+							className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+							role="alert"
+						>
+							<span className="block sm:inline">{error}</span>
+						</div>
+					)}
+
+					<div className="!mt-12">
+						<button
+							type="button"
+							className="py-3.5 px-7 text-sm font-semibold tracking-wider rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none"
+						>
+							Sign up
+						</button>
+					</div>
+				</form>
+			</div>
+		</main>
+	)
 }
