e78ae4913448b99475eceaa18f1448cc828da7a2
Adriano Maringolo <adrianomaringolo@gmail.com>
2024-12-07 11:58:53 -0300

    refactor: update PWA configuration to include experimental missingSuspenseWithCSRBailout option

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
-  dest: "public",
-});
+const withPWA = require('next-pwa')({
+	dest: 'public',
+})
 
 module.exports = withPWA({
-  // config
-});
+	experimental: {
+		missingSuspenseWithCSRBailout: false,
+	},
+})
